aacf0713142a9e2286673d435301bc5d3c45ff29
Claire Harris <c.harris.2@research.gla.ac.uk>
2019-12-16 17:00:20 +0000

    CHESS.jl: Load and plot rainfall to check.

--- examples/CHESS.jl
+++ examples/CHESS.jl
@@ -3,10 +3,16 @@ using UKclim
 using MyUnitful
 using Unitful
 using Unitful.DefaultSymbols
+using Plots
+gr()
 cd("/home/claireh/Documents/UK")
 ncinfo("CHESS/prec/chess_precip_201001.nc")
 
 
 times = collect(2010year:1month:2011year+9month)
 dir = "CHESS/prec1/"
-sun = readCHESS(dir, "precip", times)
+prec = readCHESS(dir, "precip", times)
+
+prec = uconvert.(kg/km^2, prec.array .* 1month)
+
+heatmap(transpose(ustrip.(prec[:,:,1])), aspect_ratio =1)
