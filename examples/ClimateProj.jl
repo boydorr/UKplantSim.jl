0dc6ae5f1d7d6f3a7ad0f9d822b3996c14cdd28d
Claire Harris <claire.harris@bioss.ac.uk>
2020-01-27 16:36:10 +0000

    ClimateProj.jl: gif of rainfall and temperature.

--- examples/ClimateProj.jl
+++ examples/ClimateProj.jl
@@ -21,6 +21,19 @@ rainfall = uconvert.(mm/month, prec.array) .* month
 anim = @animate for i=12:12:1200
     decrain = ustrip.(rainfall[:, :, (i-11):i])
     decrain = transpose(mapslices(mean, decrain, dims =3)[:, :, 1])
-    heatmap(decrain, background_color = :lightblue, background_color_outside = :white, grid = false, aspect_ratio = 1, clim = (0, 1400))
+    heatmap(decrain, background_color = :lightblue, background_color_outside = :white, grid = false, aspect_ratio = 1, clim = (0, 500))
 end
 gif(anim, "rainfall.gif", fps = 5)
+
+file = "UKCP18/tas_rcp85_land-rcm_uk_12km_01_mon_198012-208011.nc"
+times = collect(1980year+1month:1month:2080year)
+tas = readUKCP(file, "tas", times, "UK_active.jld")
+
+anim = @animate for i=12:12:1200
+    years = 1981:2080
+    yr = years[Int(i/12)]
+    dectas = ustrip.(tas.array[:, :, (i-11):i])
+    dectas = transpose(mapslices(mean, dectas, dims =3)[:, :, 1])
+    heatmap(dectas, background_color = :lightblue, background_color_outside = :white, grid = false, aspect_ratio = 1, clim = (275, 290), title = "Year $yr")
+end
+gif(anim, "temp.gif", fps = 5)
